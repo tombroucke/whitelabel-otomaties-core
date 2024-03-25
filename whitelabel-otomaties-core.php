423738a3e1431a41622392dde69677fc48c64265
Tom Broucke <tom@tombroucke.be>
2024-03-25 16:04:41 +0100

    ✨ feat: Initial commit

---
+++ whitelabel-otomaties-core.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Otomaties Whitelabel
+ * Description: Whitelabel Otomaties Core
+ * Version: 1.0
+ * Author: Tom Broucke
+ * Author URI: https://tombroucke.be
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: otomaties-whitelabel
+ * Domain Path: /languages
+ */
+
+add_filter('otomaties_whitelabel', '__return_true');
